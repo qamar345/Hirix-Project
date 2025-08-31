1d6387a378f6b39ac86abc2b2a24554ab9306d1b
Qamar Naveed <80244665+qamar345@users.noreply.github.com>
2025-08-31 05:42:50 +0500

    Update app.js

--- hirix-backend/app.js
+++ hirix-backend/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https:jobs.hirix.pk", // frontend URL
+    origin: "https://jobs.hirix.pk", // frontend URL
     credentials: true,
   })
 );
